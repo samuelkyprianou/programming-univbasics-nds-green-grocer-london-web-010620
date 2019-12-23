8fd75a87fe69d4049579900a576a3c2a0c5e0c38
Sam Kyprianou <samuelkyprianou@gmail.com>
2019-12-23 16:44:30 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -15,7 +15,7 @@ def consolidate_cart(cart)
 new_cart = []
 d = 0 
 while d < cart.length do
-  new_cart_item = find_item_by_name_in_collection(cart[d])
+  new_cart_item = find_item_by_name_in_collection(cart[d][:item], )
 end
 
 def apply_coupons(cart, coupons)
