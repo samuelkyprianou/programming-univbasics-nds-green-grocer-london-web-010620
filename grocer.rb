f0f535e9f28d42ed7c7c99ca688fd67a56014818
Sam Kyprianou <samuelkyprianou@gmail.com>
2019-12-23 17:27:38 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -62,7 +62,7 @@ def apply_clearance(cart)
 d = 0 
 while d < cart.length do
   if cart[d][:clearance]
-    cart[d][:price] = cart[d][:price] - (cart[d][:price])
+    cart[d][:price] = cart[d][:price] - (cart[d][:price] * )
   end
   d += 1 
 end
