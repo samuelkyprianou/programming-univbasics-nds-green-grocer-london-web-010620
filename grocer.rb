4a5c7b2feb1ffe2a0610eec779830a9dc77aaf85
Sam Kyprianou <samuelkyprianou@gmail.com>
2019-12-23 17:30:36 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -77,7 +77,7 @@ final_cart = apply_clearance(coupon_cart)
 d = 0 
 total = 0
 while d < final_cart.length do
-  total += final_cart[d][:price] * [final_cart][d][:count]
+  total += final_cart[d][:price] * final_cart[d][:count]
   d += 1
 end
 if total > 100
