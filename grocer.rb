331ecb1ed6b6a123666998534990bf64fa618b8b
Sam Kyprianou <samuelkyprianou@gmail.com>
2019-12-23 16:55:16 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -40,7 +40,7 @@ while d < coupons.length do
   cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart)
   if cart_item && cart_item[:count] >= coupons[d][:num]
   if cart_item_with_coupon
-    cart_item_with_coupon
+    cart_item_with_coupon[:count] 
     
 end
 
