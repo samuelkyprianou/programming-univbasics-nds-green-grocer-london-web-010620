49c12a0d93ead47a66360f9fa72b86842b6e0686
Sam Kyprianou <samuelkyprianou@gmail.com>
2019-12-23 17:10:12 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -70,13 +70,8 @@ cart
 end
 
 def checkout(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # This method should call
-  # * consolidate_cart
-  # * apply_coupons
-  # * apply_clearance
-  #
-  # BEFORE it begins the work of calculating the total (or else you might have
-  # some irritated customers
+checkout_cart = consolidate_cart(cart)
+apply_coupons(checkout_cart, coupons)
+apply_clearance(checkout_cart)
 end
+return checkout_cart
