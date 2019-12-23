1e9abbc036da00990d1def84b6a2fe06e8407d08
Sam Kyprianou <samuelkyprianou@gmail.com>
2019-12-23 16:53:39 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -38,7 +38,7 @@ while d < coupons.length do
   cart_item = find_item_by_name_in_collection(coupons[d][:item], cart)
   couponed_item_name = "#{coupons[d][:item]} W/COUPON"
   cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart)
-  if cart_item && 
+  if cart_item && cart_item[:count] 
   if cart_item_with_coupon
     
 end
